d9e0980b24e6ce94142a6fe6c6fe9a1949b1dba2
mekbibDev <mekbibtare@gmail.com>
2024-01-03 10:02:32 +0300

    Serve different html depending on the url

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const fs = require("node:fs/promises");
 const hostname = "localhost";
 const port = 8080;
 const server = http.createServer(async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  const html = await fs.readFile("./index.html");
-  res.write(html);
+  const path = req.url === "/" ? "./index.html" : `.${req.url}.html`;
+  if (path !== "/favicon.ico") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+
+    let html;
+    try {
+      html = await fs.readFile(path);
+    } catch (error) {
+      html = await fs.readFile("./404.html");
+    }
+    res.write(html);
+  }
   res.end();
 });
 
